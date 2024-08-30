b0b3209bb56dff08a936a8fc6ea79aad4c87ee8d
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-08-30 11:24:06 +0100

    Add project files.

---
+++ JQueryExample/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JQueryExample.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ JQueryExample/Models/Pizza.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JQueryExample.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+
+    [DataType(DataType.Currency)]
+    public float Price { get; set; }
+
+    public string Toppings { get; set; }
+}
